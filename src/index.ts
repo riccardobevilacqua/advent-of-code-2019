a26a60892d528bbe5e81ffa3682ae5b1091e4488
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2019-12-04 19:12:17 +0100

    Removed console log

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { calculateRocketEquation } from './puzzle-01/puzzle-01';
+import { calculateRocketEquation } from "./puzzle-01/puzzle-01";
 
 const result01 = calculateRocketEquation();
 
 console.log(`PUZZLE 01: ${result01}`);
 
-console.log('DONE!');
+console.log("DONE!");
--- src/puzzle-01/puzzle-01.ts
+++ src/puzzle-01/puzzle-01.ts
@@ -1,18 +1,15 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
 const calculateFuel = (input: number): number => Math.floor(input / 3) - 2;
 
 export const calculateRocketEquation = (): number => {
-    const file = readFileSync('./dist/inputs/input-01.txt', 'utf-8');
+  const file = readFileSync("./dist/inputs/input-01.txt", "utf-8");
 
-    return file
-        .split('\n')
-        .reduce((acc, cur, index) => {
-            console.log(`${index + 1}: ${parseInt(cur)}`);
-            if (cur && parseInt(cur) > 0) {
-                acc += calculateFuel(parseInt(cur));
+  return file.split("\n").reduce((acc, cur, index) => {
+    if (cur && parseInt(cur) > 0) {
+      acc += calculateFuel(parseInt(cur));
 
-                return acc;
-            }
-        }, 0);
-}
+      return acc;
+    }
+  }, 0);
+};
