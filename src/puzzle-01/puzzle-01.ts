17674651c5c4906154f4294aec11e7f511a319b4
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2019-12-04 19:14:45 +0100

    Added types

--- src/puzzle-01/puzzle-01.ts
+++ src/puzzle-01/puzzle-01.ts
@@ -3,9 +3,9 @@ import { readFileSync } from "fs";
 const calculateFuel = (input: number): number => Math.floor(input / 3) - 2;
 
 export const calculateRocketEquation = (): number => {
-  const file = readFileSync("./dist/inputs/input-01.txt", "utf-8");
+  const file: string = readFileSync("./dist/inputs/input-01.txt", "utf-8");
 
-  return file.split("\n").reduce((acc, cur, index) => {
+  return file.split("\n").reduce((acc: number, cur: string) => {
     if (cur && parseInt(cur) > 0) {
       acc += calculateFuel(parseInt(cur));
 
