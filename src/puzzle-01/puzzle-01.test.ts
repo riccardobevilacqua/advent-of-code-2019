da18be701cc2da74f3483c2925a2dfe09af1066e
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2019-12-05 22:17:56 +0100

    Added tests

--- src/puzzle-01/puzzle-01.test.ts
+++ src/puzzle-01/puzzle-01.test.ts
@@ -1,5 +1,14 @@
-import { calculateFuel } from './puzzle-01';
+import { calculateFuel, calculatePreciseFuel } from './puzzle-01';
 
 it('should calculate fuel', () => {
   expect(calculateFuel(12)).toBe(2);
+  expect(calculateFuel(14)).toBe(2);
+  expect(calculateFuel(1969)).toBe(654);
+  expect(calculateFuel(100756)).toBe(33583);
+});
+
+it('should calculate precise fuel', () => {
+  expect(calculatePreciseFuel(14)).toBe(2);
+  expect(calculatePreciseFuel(1969)).toBe(966);
+  expect(calculatePreciseFuel(100756)).toBe(50346);
 });
