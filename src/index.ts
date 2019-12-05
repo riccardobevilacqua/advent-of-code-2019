8e55681ff025d6708aecc3f2ac0b8fd724287634
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2019-12-06 00:46:09 +0100

    Added puzzle 02 solution

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { calculateEquation } from './puzzle-01/puzzle-01';
+import { getProcessedValue } from './puzzle-02/puzzle-02';
 
 const result01 = calculateEquation();
 const result01b = calculateEquation(true);
+const result02 = getProcessedValue();
 
 console.log(`PUZZLE 01 ---> PART 1: ${result01} | PART 2: ${result01b}`);
+console.log(`PUZZLE 02 ---> PART 1: ${result02}`);
 
 console.log('DONE!');
