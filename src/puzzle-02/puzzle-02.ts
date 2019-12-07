4551198863aad7e56c6f54b12651d7715948b9a6
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2019-12-07 19:04:06 +0100

    Updated puzzle 02

--- src/puzzle-02/puzzle-02.ts
+++ src/puzzle-02/puzzle-02.ts
@@ -1,19 +1,30 @@
-import { readInput } from '../common/readInput';
+import { readInput } from "../common/readInput";
 
-export const setupIntCode = (input: number[] = []): number[] => {
+export interface IntCodeConfig {
+  input?: number[];
+  noun?: number;
+  verb?: number;
+}
+
+export const setupIntCode = (
+  { input, noun, verb }: IntCodeConfig = { input: [] }
+): number[] => {
   if (input.length === 0) {
-    input = readInput('./dist/inputs/input-02.txt', ',');
+    input = readInput("./dist/inputs/input-02.txt", ",");
   }
 
-  input[1] = 12;
-  input[2] = 2;
+  input[1] = noun ? noun : 12;
+  input[2] = verb ? verb : 2;
 
   return [...input];
 };
 
-export const runIntCode = (input: number[] = [], index: number = 0): number[] => {
+export const runIntCode = (
+  input: number[] = [],
+  index: number = 0
+): number[] => {
   if (input.length === 0) {
-    input = readInput('./dist/inputs/input-02.txt', ',');
+    input = readInput("./dist/inputs/input-02.txt", ",");
   }
 
   let intCode: number[] = [...input];
@@ -21,17 +32,21 @@ export const runIntCode = (input: number[] = [], index: number = 0): number[] =>
   if (intCode[index] === 99) {
     return intCode;
   } else if (intCode[index] === 1) {
-    intCode[intCode[index + 3]] = intCode[intCode[index + 1]] + intCode[intCode[index + 2]];
+    intCode[intCode[index + 3]] =
+      intCode[intCode[index + 1]] + intCode[intCode[index + 2]];
   } else if (intCode[index] === 2) {
-    intCode[intCode[index + 3]] = intCode[intCode[index + 1]] * intCode[intCode[index + 2]];
+    intCode[intCode[index + 3]] =
+      intCode[intCode[index + 1]] * intCode[intCode[index + 2]];
   }
 
   return runIntCode(intCode, index + 4);
 };
 
-export const getProcessedValue = () => {
+export const getProcessedValue = (): number => {
   const input: number[] = setupIntCode();
   const output: number[] = runIntCode(input);
 
   return output[0];
 };
+
+export const getGravityAssist = () => {};
