c84f185914d349c067c30f991d90d539b6e30456
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2019-12-08 23:13:23 +0100

    Updated eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,20 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ["eslint:recommended"],
+  extends: ['eslint:recommended'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module'
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    quotes: ["error", "single", { allowTemplateLiterals: true }],
-    semi: ["error", "always"],
-    indent: ["error", 2]
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: ['error', 'always'],
+    indent: ['error', 2]
   }
 };
